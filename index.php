fc38f7009686b85cd38f6466258aa48ed39bdae2
Jordan Potter <jmp0037@mix.wvu.edu>
2020-11-19 18:23:47 -0500

    Pre Presentation

--- index.php
+++ index.php
@@ -2,34 +2,35 @@
 require 'includes/header.php'
 ?>
 <main>
-<link rel="stylesheet" href="css/about.css">
+<link rel="stylesheet" href="css/index.css">
     <title>Video Game Warehouse: Find video games at a store near you</title>
-    <button class="account-button">Login</button>
-    <button class="account-button">Sign Up</button>
+    <!-- <button class="account-button">Login</button>
+    <button class="account-button">Sign Up</button> -->
+    <h1 style="font-family: Impact, Charcoal, sans-serif; justify-content: center;">Welcome to Video Game Warehouse</h1>
     <p style=" font-family: Verdana, Geneva, Tahoma, sans-serif">At Video Game Warehouse, you can find a variety of
         games that are available in our many store locations. Below
         you will see all available games, as well as their prices. Please remember to read the disclaimers.</p>
-    <h1 style="font-family: Impact, Charcoal, sans-serif">PS4 Games</h1>
+    <h2 style="font-family: Impact, Charcoal, sans-serif; justify-content: center;">Average Game Prices by Platform</h1>
     <div class="row">
         <div class="game" style="float: left">
-            <img src="images/assassinsCreed.jpg">
-            <p class="price">Price: $49.94</p>
+            <img src="images/carousel1.jpg">
+            <p class="price">Price Range: $60-$70</p>
         </div>
         <div class="game" style="float: left">
-            <img src="images/cyberpunk.jpg">
-            <p class="price">Price: $49.94</p>
+            <img src="images/carousel2.jpg">
+            <p class="price">Price Range: $60-$70</p>
         </div>
         <div class="game" style="float: left">
-            <img src="images/hzd.jpg">
-            <p class="price">Price: $16.99</p>
+            <img src="images/carousel3.jpg">
+            <p class="price">Price Range: $40-$60</p>
         </div>
         <div class="game">
-            <img src="images/spiderman.jpg">
-            <p class="price">Price: $32.90</p>
+            <img src="images/carousel4.jpg">
+            <p class="price">Price Range: Variable</p>
         </div>
     </div>
     <div class="disclaimers">
-        <h2>Disclaimers:</h2>
+        <h2 style="font-family: Impact, Charcoal, sans-serif">Disclaimers:</h2>
         <ul>
             <li>Quantity limited. Available while supply lasts</li>
             <li>Prices may differ in store due to sales</li>
